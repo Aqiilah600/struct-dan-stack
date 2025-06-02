f1b3358dd5aa5cd6919aad73dee8a5910465b2f8
Aqiilah <aqiilahrozi@gmail.com>
2025-06-02 20:19:56 +0800

    first commit

---
+++ Struct.cpp
@@ -0,0 +1,22 @@
+#include <conio.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+ struct mahasiswa{
+	string nim;
+  string nama;
+  string alamat;
+  float ipk;
+};
+
+mahasiswa doni;
+
+Aqiilah.nim="C030324010";
+Aqiilah.nama="'Aqiilah";
+Aqiilah.alamat="JL.Tatah Bangkal No.138";
+Aqiilah.ipk=3.32;
+
+cout << Aqiilah.nim<<"\n";
+cout << Aqiilah.nama<<"\n";
+	
