99d7962d33879c400490e4facdb45814b006cc6c
Aqiilah <aqiilahrozi@gmail.com>
2025-06-03 15:59:28 +0800

    penambahan Data

--- Stack.cpp
+++ Stack.cpp
@@ -16,7 +16,33 @@ void initializeStack(Stack *S){
 int Full(Stack *S){
     return (S->Count == MAXSATCK);
 }
-
 int Empty(Stack *S){
     return (S->Count == 0);
+}
+
+int main(){
+    Stack Gudang;
+    itemstack *B = Gudang.Barang;
+
+    initializeStack(&Gudang);
+
+    Pop(&Gudang, B);
+    Push(35, &Gudang);
+    
+    Push(56, &Gudang);
+    Push(13, &Gudang);
+    Push(12, &Gudang);
+    Pop(&Gudang, B);
+
+    Push(76, &Gudang);
+    Push(34, &Gudang);
+    Push(87, &Gudang);
+    Push(99, &Gudang);
+    Push(10, &Gudang);
+    Push(45, &Gudang);
+    Push(54, &Gudang);
+    Push(71, &Gudang);
+    
+    Pop(&Gudang, B);
+    Push(32, &Gudang);
 }
