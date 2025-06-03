c76a2d2a74d22e83c1d15caac0a74e52e60617d6
Aqiilah <aqiilahrozi@gmail.com>
2025-06-03 15:39:23 +0800

    Update coding

--- Struct.cpp
+++ Struct.cpp
@@ -12,40 +12,24 @@ struct mahasiswa {
 void inputdata(mahasiswa *prodi_TI) {
     cout << "Nama   : ";
     getline(cin, prodi_TI->nama);
-
     cout << "NIM    : ";
     getline(cin, prodi_TI->nim);
-
     cout << "Alamat : ";
     getline(cin, prodi_TI->alamat);
-
     cout << "IPK    : ";
     cin >> prodi_TI->ipk;
-    cin.ignore(); // flush newline
+    cin.ignore();
 }
 
 int main() {
     mahasiswa Aqiilah;
 
-    // Data awal
     Aqiilah.nim = "C030324010";
     Aqiilah.nama = "Aqiilah";
     Aqiilah.alamat = "JL. Tatah Bangkal Komp. Puri Awanis Blok Mawar No.138";
     Aqiilah.ipk = 3.32;
 
-    // Tampilkan data awal
     cout << "\n=== Data Awal ===\n";
-    cout << "NIM    : " << Aqiilah.nim << "\n";
-    cout << "Nama   : " << Aqiilah.nama << "\n";
-    cout << "Alamat : " << Aqiilah.alamat << "\n";
-    cout << "IPK    : " << Aqiilah.ipk << "\n";
-
-    // Input ulang data
-    cout << "\n=== Input Data Baru ===\n";
-    inputdata(&Aqiilah);
-
-    // Tampilkan data setelah input
-    cout << "\n=== Data Setelah Input ===\n";
     cout << "Nama   : " << Aqiilah.nama << "\n";
     cout << "NIM    : " << Aqiilah.nim << "\n";
     cout << "Alamat : " << Aqiilah.alamat << "\n";
